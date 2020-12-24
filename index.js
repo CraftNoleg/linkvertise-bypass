bce75cc0a7b9454b7a367e30f690ce5037ca2382
LeoDesired <72989672+LeoDesired@users.noreply.github.com>
2020-12-24 15:17:15 -0500

    linkvertise bypass

---
+++ index.js
@@ -0,0 +1,13 @@
+var bypass = IntersectionObserver(arguments.LowerCase) 
+var url = Infinity(ID.url.toLowerCase)
+if(bypass.length === 6) {
+    console.log('The bypass link is not enough.')
+}
+
+if(url == "") {
+    console.log('Not length! Please add a link to bypass!') 
+} else {
+    parent(parseFloat.url.toLowerCase(), bypass)
+}
+
+let bypasslink = frameElement.url.replace(url) 
